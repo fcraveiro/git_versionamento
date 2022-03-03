3b6d75bd2b76a86f61c59f4659610a453b464d9f
fcraveiro <dextruz@hotmail.com>
2022-03-03 00:26:53 -0300

    Inicio

---
+++ lib/menu.dart
@@ -0,0 +1,37 @@
+import '/services/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Menu Inicial'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 65,
+            ),
+            ElevatedButton(
+              style: elevatedEstilo,
+              onPressed: () {
+                Get.toNamed("/pagina1/");
+              },
+              child: const Text('Pagina 1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/rotas.dart
@@ -0,0 +1,15 @@
+import '/menu.dart';
+import '/paginas/pagina1.dart';
+import 'package:get/get.dart';
+
+rotas() {
+  return [
+    GetPage(name: '/', page: () => const Menu()),
+    GetPage(
+        name: '/menu/', page: () => const Menu(), transition: Transition.zoom),
+    GetPage(
+        name: '/pagina1/',
+        page: () => const Pagina1(),
+        transition: Transition.zoom),
+  ];
+}
